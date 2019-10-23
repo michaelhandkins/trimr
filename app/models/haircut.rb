63560c2f184087d75585dcaf8264b36c08713f42
Michael Handkins <michaelhandkins@aol.com>
2019-10-23 00:26:49 +0000

    Made all fields for haircuts required

--- app/models/haircut.rb
+++ app/models/haircut.rb
@@ -1,3 +1,7 @@
 class Haircut < ApplicationRecord
   belongs_to :barber
+
+  validates :haircut_name, presence: true
+  validates :description, presence: true
+  validates :cost, presence: true
 end
